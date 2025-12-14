12d7f544829c9819e27050dbfbee2e623cd10495
Rebilaz <zrebila@gmail.com>
2025-12-14 21:34:34 +0100

    1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/labs/saas1/:path*",
+        destination: "https://saas1-gray.vercel.app/labs/saas1/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
