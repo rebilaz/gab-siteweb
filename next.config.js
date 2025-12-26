0032f3ea32f1b508e166299aba8b749458e73484
Rebilaz <zrebila@gmail.com>
2025-12-26 13:05:14 +0100

    1

--- next.config.js
+++ next.config.js
@@ -1,13 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
+    remotePatterns: [{ protocol: "https", hostname: "**" }],
+  },
+  async redirects() {
+    return [
       {
-        protocol: "https",
-        hostname: "**",
+        source: "/:path*",
+        has: [{ type: "host", value: "vexly.fr" }],
+        destination: "https://www.vexly.fr/:path*",
+        permanent: true,
       },
-    ],
+    ];
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
